d1b1a101931b4f66191d87f656a8b6f829d693d8
Prokhor <wolfstudio.2025.2026@gmail.com>
2026-07-04 21:11:47 +0300

    Add Grid controller and Create field

---
+++ Assets/Scripts/GameGrid.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameGrid : MonoBehaviour
+{
+    public int columnLength, rowLength;
+
+    public float x_Space, z_Space;
+
+    public GameObject grass;
+
+    public GameObject[] currentGrid;
+
+    public bool gotGrid;
+
+    public GameObject hitted;
+
+    public GameObject field;
+
+    private RaycastHit _hit;
+
+    public bool creatingFields;
+
+    private void Start()
+    {
+        for (int i = 0; i < columnLength * rowLength; i++)
+        {
+            Instantiate(grass, new Vector3(x_Space + (x_Space * (i % columnLength)), 0, z_Space + (z_Space * (i / columnLength))), Quaternion.identity, transform);
+
+
+        }
+    }
+
+    private void Update()
+    {
+        if (gotGrid == false)
+        {
+            currentGrid = GameObject.FindGameObjectsWithTag("grid");
+
+            gotGrid = true;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out _hit))
+            {
+                if (creatingFields == true)
+                {
+                    if (_hit.transform.tag == "grid")
+                    {
+                        hitted = _hit.transform.gameObject;
+                        Instantiate(field, hitted.transform.position, Quaternion.identity);
+                        Destroy(hitted);
+                    }
+                }
+            }
+        }
+    }
+
+    public void CreateFields()
+    {
+        creatingFields = true;
+    }
+
+    public void returnToNormality()
+    {
+        creatingFields = false;
+    }
+}
